818b666b77238c847b413b6869051109df255378
Florin Uscatu <florinusc@gmail.com>
2021-02-20 12:59:49 +0200

    Initial project setup
    Create rocket list view controller

---
+++ Rockets/Views/RocketListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RocketListViewController.swift
+//  Rockets
+//
+//  Created by Florin Uscatu on 20.02.2021.
+//
+
+import UIKit
+
+class RocketListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup() {
+        title = "Rockets"
+    }
+    
+}
